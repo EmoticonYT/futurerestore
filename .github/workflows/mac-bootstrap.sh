c2cec6f0fca0cf53626e2dfcb778da084cd36c94
emoticonyt <doriandelvalle67@gmail.com>
2026-06-17 13:32:33 -0500

    Update mac-bootstrap.sh

--- .github/workflows/mac-bootstrap.sh
+++ .github/workflows/mac-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 set -e
-export WORKFLOW_ROOT=/Users/runner/work/futurerestore-nightly/futurerestore-nightly/.github/workflows
+export WORKFLOW_ROOT=/Users/runner/work/futurerestore/futurerestore/.github/workflows
 export DEP_ROOT=/Users/runner/work/futurerestore/futurerestore/dep_root
 export BASE=/Users/runner/work/futurerestore/futurerestore/
 
